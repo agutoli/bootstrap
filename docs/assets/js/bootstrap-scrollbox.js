eb23421031b5c6efc9a5371e9f5b19c54f696226
Bruno Thiago Leite Agutoli <brunotla1@gmail.com>
2012-11-01 20:18:48 -0200

    :)

--- docs/assets/js/bootstrap-scrollbox.js
+++ docs/assets/js/bootstrap-scrollbox.js
@@ -27,150 +27,248 @@
  /* scrollbox PUBLIC CLASS DEFINITION
   * =============================== */
 
-  var scrollbox = function (element, options) {
+  var Scrollbox = function (element, options) {
     this.init('scrollbox', element, options);
-  }
+  };
 
-  scrollbox.prototype = {
+  Scrollbox.prototype = {
 
-    constructor: scrollbox,
+    constructor: Scrollbox,
 
     init: function (type, element, options) {
-      
-      var _this = this;
 
       this.type = type;
+
+      //selectors
       this.$element = $(element);
+      this.validate();
       this.$scroll_inner = $('.scrollbox-inner', this.$element);
       this.$button_prev = $('.scrollbox-control-prev', this.$element);
       this.$button_next = $('.scrollbox-control-next', this.$element);
+      this.$scroll_ul = $('ul', this.$scroll_inner);
+      this.$scroll_rows = $('ul li', this.$scroll_inner); 
+
       this.options = this.getOptions(options);
       this.enabled = true;
 
-      //events
+      //add events
       this.$button_prev.on('click.' + this.type, this.options.selector, $.proxy(this.prev, this));
       this.$button_next.on('click.' + this.type, this.options.selector, $.proxy(this.next, this));
 
-      this.rows = $('ul li', this.$scroll_inner); 
-      this.corrent_row = 0;
-
-      this.sum_rows_height = 0;
-      this.num_rows = this.rows.length;
-      this.ul_height = this.$scroll_inner.find('ul').height();
-
-      $.each(this.rows, function(i, el){
-         _this.sum_rows_height += $(el).height(); 
-      });
-
+      this.smartEndAdjustment = 0;
+      this.corrent_index_row = 0;
+      this.scroll_position = 0;
+      this.num_rows = this.$scroll_rows.length;
+      this.ul_height = this.$scroll_ul.height();
       this.inner_height = this.$scroll_inner.height();
-      this.line_height = (this.ul_height - this.sum_rows_height) / (this.num_rows + 1);
-      this.scroll_height = 0;
-      this.$button_prev.find('.btn').addClass('disabled');
+      this.line_height = this.getLineHeight();
+
+      //disabled previous button 
+      this.disabledPrevButton();
+      this.updateCorrentIndex();
     },
 
+    corrent: 0,
+
     next: function() {
-      var _this = this;
-      var row_corrent_height = 0;
-      for(var offset = this.corrent_row; offset < this.num_rows; offset++) {
-        var row_height = $(this.rows[offset]).height();
-        if (this.inner_height > (row_corrent_height + row_height + this.line_height) ) {
-          row_corrent_height += row_height + this.line_height;
-          this.scroll_height += row_height + this.line_height;
-          this.corrent_row = offset;
+
+      this.corrent++;
+
+      var scroll_inner_offset = this.$scroll_inner.offset();
+      var $corrent_row = $(this.$scroll_rows[this.corrent]);
+      var scroll_row_offset = $corrent_row.offset();
+
+      if (this.corrent == (this.num_rows-1) ) {
+        var offset_inner_bottom = scroll_inner_offset.top + this.$scroll_inner.height();
+        console.log(offset_inner_bottom);
+        //this.scroll_position += offset_inner_bottom - $corrent_row.height() + this.line_height;
+        //var t = scroll_row_offset.top + $corrent_row.height() + this.line_height;
+        //console.log(scroll_inner_offset.top, scroll_row_offset.top, t);
+      } else {
+        this.scroll_position += (scroll_inner_offset.top - scroll_row_offset.top) + this.line_height;
+      }
+
+      this.enabledPrevButton();
+      this.moveVerticalScroller();
+      this.updateCorrentIndex();
+
+
+
+
+      /*var sum_row_height = 0;
+      
+      if (this.corrent_index_row > 0) {
+        this.corrent_index_row++;
+      }
+
+      for(var offset = this.corrent_index_row; offset < this.num_rows; offset++) {
+        var row_height = $(this.$scroll_rows[offset]).height();
+        if ( this.checkIfSumOfLinesFitOnTheDisplay(sum_row_height, row_height) ) {
+          sum_row_height += this.sumHeightAndLineHeight(row_height);
+          this.scroll_position += this.sumHeightAndLineHeight(row_height);
+          this.corrent_index_row = offset;
         }
       }
 
-      var diff_end_scroller = (this.ul_height - this.scroll_height) - this.inner_height;
-      if (diff_end_scroller < 0) {
-        this.scroll_height += diff_end_scroller;
-        this.corrent_row = this.num_rows - 1;
-        this.$button_next.find('.btn').addClass('disabled');
+      this.smartEndAdjustment = (this.ul_height - this.scroll_position) - this.inner_height;
+
+      if (this.smartEndAdjustment < 0) {
+        this.scroll_position += this.smartEndAdjustment;
+        this.corrent_index_row++;
+        this.disabledNextButton();
       }
 
-      var prev = this.$button_prev.find('.btn');
-      if (prev.hasClass('disabled')) prev.removeClass('disabled');
-
-      this.$scroll_inner.find('ul').animate({top: '-'+ _this.scroll_height});
+      this.enabledPrevButton();
+      this.moveVerticalScroller();*/
     },
 
     prev: function() {
-      var _this = this;
-      var row_corrent_height = 0;
+     /*
+     var sum_row_height = 0;
 
-      for(var offset = this.corrent_row; offset >= 0; offset--) {
-        var row_height = $(this.rows[offset]).height();
-        if (this.inner_height > (row_corrent_height + row_height + this.line_height) ) {
-          row_corrent_height += row_height + this.line_height;
-          this.scroll_height -= row_height + this.line_height;
+      for(var offset = this.corrent_index_row; offset >= 0; offset--) {
+        var row_height = $(this.$scroll_rows[offset]).height();
+        if ( this.checkIfSumOfLinesFitOnTheDisplay(sum_row_height, row_height) ) {
+          sum_row_height += this.sumHeightAndLineHeight(row_height);
+          if (this.corrent_index_row === (this.num_rows - 1) ) {
+            this.scroll_position += this.smartEndAdjustment;
+            break;
+          } else {
+            this.scroll_position -= this.sumHeightAndLineHeight(row_height);
+          }
         }
       }
-
-      if (this.scroll_height <= 0) {
-        this.scroll_height = 0;
-        this.corrent_row = 0;
-        this.$button_prev.find('.btn').addClass('disabled');
+    
+      if (this.scroll_position <= 0) {
+        this.scroll_position = 0;
+        this.corrent_index_row = 0;
+        this.disabledPrevButton();
       }
-      
-      var next = this.$button_next.find('.btn');
-      if (next.hasClass('disabled')) next.removeClass('disabled'); 
 
-      this.$scroll_inner.find('ul').animate({top: '-'+ _this.scroll_height}, this.options.delay);
+      this.enabledNextButton();
+      this.moveVerticalScroller();*/
+    },
+
+    updateCorrentIndex: function() {
+      
+      var _this = this;
+      var sum_row_height = 0;
+
+      for(var offset = this.corrent; offset < this.num_rows; offset++) {
+        var row_height = $(this.$scroll_rows[offset]).height();
+        
+        if ( _this.checkIfSumOfLinesFitOnTheDisplay(sum_row_height, row_height) ) {
+          sum_row_height += row_height + _this.line_height;
+          _this.corrent = offset;
+        }
+      }
+      console.log('index: ',_this.corrent);
+    },
+
+    checkIfSumOfLinesFitOnTheDisplay: function(sum_row_height, row_height) {
+      if (this.inner_height > (sum_row_height + row_height + this.line_height) ) {
+        return true;
+      }
+      return false;
+    },
+
+    sumHeightAndLineHeight: function(height) {
+      return (height + this.line_height);
+    },
+
+    moveVerticalScroller: function() {
+      
+      if (!this.enabled) {
+        return false;
+      }
+
+      var scroll_top = this.scroll_position;
+
+      this.$scroll_ul.animate({
+        top: scroll_top
+      }, this.options.delay, $.proxy(this.options.completeAnimation, this, scroll_top));
     },
 
     getOptions: function (options) {
-      options = $.extend({}, $.fn[this.type].defaults, options, this.$element.data())
+      options = $.extend({}, $.fn[this.type].defaults, options, this.$element.data());
+      return options;
+    },
 
-      if (options.delay && typeof options.delay == 'number') {
-        options.delay = {
-          show: options.delay
-        , hide: options.delay
-        }
-      }
+    enabledNextButton: function() {
+      var next = this.$button_next.find('.btn');
+      next.removeClass('disabled');
+    },
 
-      return options
+    disabledNextButton: function() {
+      var next = this.$button_next.find('.btn');
+      next.addClass('disabled');
+    },
+
+    enabledPrevButton: function() {
+      var prev = this.$button_prev.find('.btn');
+      prev.removeClass('disabled');
+    },
+
+    disabledPrevButton: function() {
+      var prev = this.$button_prev.find('.btn');
+      prev.addClass('disabled');
+    },
+   
+    getLineHeight: function(){
+      var sum_rows_height = 0;
+      $.each(this.$scroll_rows, function(i, el){
+        sum_rows_height += $(el).height(); 
+      });
+      return (this.ul_height - sum_rows_height) / (this.num_rows + 1);
     },
 
     validate: function () {
       if (!this.$element[0].parentNode) {
-        this.hide()
-        this.$element = null
-        this.options = null
+        this.$element = null;
+        this.options = null;
       }
     },
 
     enable: function () {
-      this.enabled = true
+      this.enabled = true;
     },
 
     disable: function () {
-      this.enabled = false
+      this.enabled = false;
     },
 
     destroy: function () {
-      this.hide().$element.off('.' + this.type).removeData(this.type)
+      this.$element.off('.' + this.type).removeData(this.type);
+      this.$button_prev.off('.' + this.type).removeData(this.type);
+      this.$button_next.off('.' + this.type).removeData(this.type);
     }
-  }
-
+  };
 
  /* scrollbox PLUGIN DEFINITION
   * ========================= */
 
   $.fn.scrollbox = function ( option ) {
     return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('scrollbox')
-        , options = typeof option == 'object' && option
-      if (!data) $this.data('scrollbox', (data = new scrollbox(this, options)));
-      if (typeof option == 'string') data[option]();
-    })
-  }
+      var $this = $(this),
+          data = $this.data('scrollbox'),
+          options = typeof option === 'object' && option;
+      if (!data) {
+        $this.data('scrollbox', (data = new Scrollbox(this, options)));
+      }
+      if (typeof option === 'string') {
+        data[option]();
+      }
+    });
+  };
 
-  $.fn.scrollbox.Constructor = scrollbox;
+  $.fn.scrollbox.Constructor = Scrollbox;
 
   $.fn.scrollbox.defaults = {
     animation: true,
     selector: false,
-    delay: 'slow'
+    delay: 'fast',
+    onClick: function(){},
+    completeAnimation: function(){}
   };
 
 }(window.jQuery);
